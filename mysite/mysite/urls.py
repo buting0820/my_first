dc2055d1dc96ba4fe250c0e40872d57cf3514134
caoyang <caoyang@klicen.com>
2019-06-04 12:39:32 +0800

    后端APP代码更新

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .polls import views
+from mysite.mysite.polls import views
 
 # 我的web项目
 urlpatterns = [
